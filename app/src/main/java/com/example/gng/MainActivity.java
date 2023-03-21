266c1d34caa3d27ca8157283fdb8479e3af37db8
yashchawla604 <33857447+yashchawla604@users.noreply.github.com>
2023-03-21 16:48:56 -0700

    Activity linking with error removal

--- app/src/main/java/com/example/gng/MainActivity.java
+++ app/src/main/java/com/example/gng/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void loginFunction(View view) {
+        Intent intent = new Intent(MainActivity.this, loginPage.class);
+        startActivity(intent);
+    }
+
+    public void signUpFunction(View view) {
+        Intent intent = new Intent(MainActivity.this, signUpPage.class);
+        startActivity(intent);
     }
 }
