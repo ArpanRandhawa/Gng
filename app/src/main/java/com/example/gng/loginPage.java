c69a2eb2bb16194f8760298abf274683ccd9990e
yashchawla604 <33857447+yashchawla604@users.noreply.github.com>
2023-03-21 17:09:10 -0700

    updates

--- app/src/main/java/com/example/gng/loginPage.java
+++ app/src/main/java/com/example/gng/loginPage.java
@@ -2,7 +2,9 @@ package com.example.gng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class loginPage extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class loginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
     }
+
+    public void loginToMain(View view) {
+        Intent intent = new Intent(loginPage.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void loginToSignUp(View view) {
+        Intent intent = new Intent(loginPage.this, signUpPage.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/gng/signUpPage.java
+++ app/src/main/java/com/example/gng/signUpPage.java
@@ -2,7 +2,9 @@ package com.example.gng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class signUpPage extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class signUpPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
     }
+    public void signUpFunction(View view) {
+        Intent intent = new Intent(signUpPage.this, loginPage.class);
+        startActivity(intent);
+    }
+
 }
